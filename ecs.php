c09447972d394ce255776490dd28c93f757c33bf
Fred <frederic@dedi-agency.com>
2025-01-14 15:04:53 +0100

    feat: bump sylius to 2.0
    BREAKING CHANGE: bump sylius to 2.0

--- ecs.php
+++ ecs.php
@@ -1,12 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (\Symplify\EasyCodingStandard\Config\ECSConfig $containerConfigurator): void {
-    $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests/Behat',
+        __DIR__ . '/ecs.php',
+    ]);
 
-    $containerConfigurator->parameters()->set(Option::SKIP, [
+    $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
+
+    $ecsConfig->skip([
         VisibilityRequiredFixer::class => ['*Spec.php'],
     ]);
 };
+
