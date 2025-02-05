fa663ed2e0bf0ec318d6f314ec9e3cd5e5651548
Fred <frederic@dedi-agency.com>
2025-02-05 16:16:42 +0100

    feat: type migration.

--- src/Migrations/Version20250114142339.php
+++ src/Migrations/Version20250114142339.php
@@ -23,6 +23,24 @@ final class Version20250114142339 extends AbstractMigration
             INSERT INTO dedi_sylius_seo_content_robots (seo_content_id, locale_code, is_not_indexable)
             SELECT translatable_id, locale, seo_not_indexable FROM dedi_sylius_seo_content_translation
         ');
+        $this->addSql('
+            UPDATE dedi_sylius_seo_content SET `type` = "product"
+            WHERE id IN (
+                SELECT referenceableContent_id FROM sylius_product WHERE referenceableContent_id IS NOT NULL
+            )
+        ');
+        $this->addSql('
+            UPDATE dedi_sylius_seo_content SET `type` = "taxon"
+            WHERE id IN (
+                SELECT referenceableContent_id FROM sylius_taxon WHERE referenceableContent_id IS NOT NULL
+            )
+        ');
+        $this->addSql('
+            UPDATE dedi_sylius_seo_content SET `type` = "channel"
+            WHERE id IN (
+                SELECT referenceableContent_id FROM sylius_channel WHERE referenceableContent_id IS NOT NULL
+            )
+        ');
     }
 
     public function down(Schema $schema): void
